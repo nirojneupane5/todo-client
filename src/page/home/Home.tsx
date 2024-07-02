32da6c6ef2e6d29474b1ff08e0978f3271a8fde9
nirojneupane5 <nirojneupane5@gmail.com>
2024-07-02 21:04:05 +0545

    Display task

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,9 +1,13 @@
 import AddTodo from "./AddTodo";
+import ViewTodo from "./ViewTodo";
 
 const Home = () => {
   return (
     <div className="max-w-[1320px] mx-auto px-2 py-2">
-      <AddTodo />
+      <div className="flex justify-between">
+        <AddTodo />
+        <ViewTodo />
+      </div>
     </div>
   );
 };
