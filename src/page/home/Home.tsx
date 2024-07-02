8d44c3d1160ccd99e7a227fb5a290dcf0262e9f2
nirojneupane5 <nirojneupane5@gmail.com>
2024-07-02 19:09:08 +0545

    Todo form created with validation using zod

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,5 +1,11 @@
+import AddTodo from "./AddTodo";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="max-w-[1320px] mx-auto px-2 py-2">
+      <AddTodo />
+    </div>
+  );
 };
 
 export default Home;
