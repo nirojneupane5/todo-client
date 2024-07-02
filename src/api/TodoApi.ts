7f58e4be03873629882cea67c355877b9fbcbd5c
nirojneupane5 <nirojneupane5@gmail.com>
2024-07-02 20:41:34 +0545

    Display todo api created with types

--- src/api/TodoApi.ts
+++ src/api/TodoApi.ts
@@ -4,8 +4,21 @@ type TodoForm={
     desc:string
 }
 
+export type TodoResponse={
+    id:number;
+    test_name:string;
+    desc:string;
+    status:boolean
+}[]
+
 //Route 1: Add todo
 export const addTodo=async(data:TodoForm)=>{
     const response= await axios.post("http://127.0.0.1:8000/todo/",data)
     return response.data
+}
+
+//Route 2: Dsiplay todo
+export const displayTodo=async()=>{
+    const response=await axios.get("http://127.0.0.1:8000/todo/");
+    return response.data;
 }
