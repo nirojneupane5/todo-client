690edb7127e46e675537c769625f6b5a0740daed
nirojneupane5 <nirojneupane5@gmail.com>
2024-07-09 11:40:23 +0545

    Password and Email validation added

--- src/page/user/Register.tsx
+++ src/page/user/Register.tsx
@@ -11,36 +11,62 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useMutation, useQueryClient } from "react-query";
+import { registerUser } from "@/api/UserApi";
 
-const formSchema = z.object({
-  first_name: z.string().min(2, {
-    message: "First Name must be at least 2 characters.",
-  }),
-  last_name: z.string().min(2, {
-    message: "Last Name must be at least 2 characters.",
-  }),
-  email: z.string().min(2, {
-    message: "Email must be at least 2 characters.",
-  }),
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  password: z.string().min(2, {
-    message: "Please enter the password",
-  }),
-  password2: z.string().min(2, {
-    message: "Please enter the confirm password.",
-  }),
-});
+const formSchema = z
+  .object({
+    first_name: z.string().min(2, {
+      message: "First Name must be at least 2 characters.",
+    }),
+    last_name: z.string().min(2, {
+      message: "Last Name must be at least 2 characters.",
+    }),
+    email: z.string().email({ message: "Invalid Email format" }),
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" })
+      .refine((value) => /[A-Z]/.test(value), {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .refine((value) => /[a-z]/.test(value), {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
+        message: "Password must contain at least one special character",
+      }),
+    password2: z.string(),
+  })
+  .refine((data) => data.password === data.password2, {
+    path: ["password2"],
+    message: "Confirm password does not match",
+  });
+
 const Register = () => {
+  const queryClient = useQueryClient();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      email: "",
+      password: "",
+      password2: "",
+      first_name: "",
+      last_name: "",
+    },
+  });
+  const mutation = useMutation({
+    mutationFn: registerUser,
+    onSuccess: () => {
+      form.reset();
+      queryClient.invalidateQueries({ queryKey: ["user"] });
     },
   });
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    mutation.mutate(values);
   };
   return (
     <div>
