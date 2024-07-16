f505b5b4bb6e1012769aad957ae4ea6bc50b6e85
nirojneupane5 <nirojneupane5@gmail.com>
2024-07-16 17:04:04 +0545

    DeleteTodo and UpdateTodo flex

--- src/page/todo/ViewSingleTodo.tsx
+++ src/page/todo/ViewSingleTodo.tsx
@@ -13,8 +13,10 @@ const ViewSingleTodo = ({ id, task_name, desc }: TViewSingleTodoProps) => {
       <div className="bg-blue-600 text-white px-2 py-2 rounded-xl">
         <h1 className="text-2xl font-bold capitalize">{task_name}</h1>
         <p className="text-xl font-semibold">{desc}</p>
-        <DeleteTodo id={id} />
-        <UpdateTodo id={id} task_name={task_name} desc={desc} />
+        <div className="flex items-center gap-5 py-1">
+          <DeleteTodo id={id} />
+          <UpdateTodo id={id} task_name={task_name} desc={desc} />
+        </div>
       </div>
     </div>
   );
