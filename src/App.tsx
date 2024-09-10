af7fcc30b10598b4f2b7a08a766d25c29e6294dd
nirojneupane10 <niroj.neupane@varicon.com.au>
2024-09-10 08:34:24 +0545

    test

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,7 @@ const router = createBrowserRouter(
 const queryClient = new QueryClient();
 
 function App() {
+  //test comment
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
